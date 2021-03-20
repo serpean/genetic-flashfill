d0c9ddb548c1b7aedb59cd438812aad70bd6db4a
Sergio Pérez <sergio.perez@seidor.es>
2021-03-20 19:59:14 +0100

    add rest inferface

--- app.py
+++ app.py
@@ -1,27 +1,53 @@
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, render_template
+
+from flasgger import Swagger
+
+from geneticlib import getrankfunction, evolve
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
-app.debug = True
-app.reloader = True
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/process', methods=['GET', 'POST'])
+@app.route('/process', methods=['POST'])
 def process():
-    if request.method == 'GET':
-        return jsonify({'method': 'GET', 'handler': 'process'})
-
-    elif request.method == 'POST':
+    """Process enpoint doc
+       ---
+       definitions:
+         Example:
+           type: array
+           items:
+            string
+         Question:
+           type: string
+       parameters:
+         - name: body
+           in: body
+           type: Object
+           properties:
+            examples:
+                type: array
+                items:
+                    $ref: "#/definitions/Example"
+            questions:
+                type: array
+                items:
+                    $ref: "#/definitions/Question"
+           required: true
+       responses:
+         200:
+           description: Answer to Questions' array
+           examples:
+             answer: ['string']
+       """
+    if request.method == 'POST':
         content = request.json
+        print(content)
         if content:
-            return jsonify({'method': 'POST', 'handler': 'process', 'content': content})
+            rf = getrankfunction(content["examples"])
+            winner = evolve(1, 'str', 500, rf, mutationrate=0.2, breedingrate=0.1, pexp=0.7, pnew=0.1)
+            return jsonify({"answer": [winner.evaluate([i]) for i in content["questions"]]})
         else:
             abort(400, description="'content' is expected")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
